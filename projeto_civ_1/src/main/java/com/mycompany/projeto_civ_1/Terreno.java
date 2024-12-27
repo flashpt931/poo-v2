1701096df7c5abbf6c2fde8766b64263706bd780
flashpt931 <162701367+flashpt931@users.noreply.github.com>
2024-12-27 00:20:03 +0000

    Update Terreno.java

--- projeto_civ_1/src/main/java/com/mycompany/projeto_civ_1/Terreno.java
+++ projeto_civ_1/src/main/java/com/mycompany/projeto_civ_1/Terreno.java
@@ -11,16 +11,26 @@ package com.mycompany.projeto_civ_1;
 public abstract class Terreno {
     private String nome;
     private char simbolo;
+    private int alimento;
+    private int material;
     
-    public Terreno(String nome, char simbolo){
+    public Terreno(String nome, char simbolo, int alimento, int material){
         this.nome = nome;
         this.simbolo = simbolo;
+        this.alimento = alimento;
+        this.material = material;
     }
      public String getNome(){
          return nome;
      }
      public char getSimbolo(){
          return simbolo;
+     }
+     public int getAlimento(){
+         return alimento;
+     }
+     public int getMaterial(){
+         return material;
      }
      public abstract int getCustoMovimento();
 
